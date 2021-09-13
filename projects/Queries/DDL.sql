ef760fb5ff2e6822fa9a609311f91d5878bd4178
Cory <cory.tinsley@revature.net>
2021-09-13 10:10:54 -0400

    adding new controllers for customer and orders

--- projects/Queries/DDL.sql
+++ projects/Queries/DDL.sql
@@ -1,7 +1,7 @@
 Create Table Customer 
 (
-CustomerID tinyint not null Identity,
-Name nvarchar(100) not null
+	CustomerID tinyint not null Identity,
+	Name nvarchar(100) not null
 
 );
 
@@ -15,3 +15,21 @@ ALTER TABLE [Order].[Order]
 ADD CONSTRAINT FK_Customer_Order
 FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID);
 
+CREATE TABLE Store
+(
+	StoreID TINYINT NOT NULL IDENTITY PRIMARY KEY,
+	Name NVARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Product
+(
+	ProductID TINYINT NOT NULL IDENTITY PRIMARY KEY,
+	Name NVARCHAR(100) NOT NULL
+);
+
+CREATE TABLE OrderProduct
+(
+	OrderID SMALLINT NOT NULL,
+	ProductID SMALLINT NOT NULL,
+	CONSTRAINT PK_TempOrder
+);
